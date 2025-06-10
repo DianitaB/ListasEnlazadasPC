87cc375242bc5ed29d8f0a1fddc2e8099eeeecc1
Diana Borja <valeriaborja975@gmail.com>
2025-06-10 08:30:52 -0500

    Model

--- src/Model/Contact.java
+++ src/Model/Contact.java
@@ -1,5 +1,27 @@
 package Model;
 
-public class Contact {
-    
+public class Contact <T,U>{
+    private T name;
+    private U phone;
+    public T getName() {
+        return name;
+    }
+    public void setName(T name) {
+        this.name = name;
+    }
+    public U getPhone() {
+        return phone;
+    }   
+    public void setPhone(U phone) {
+        this.phone = phone;
+    }
+    public Contact(T name, U phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact - Name:" + name + "Phone: " + phone ;
+    }
 }
